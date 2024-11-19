384bb59939cc2cf143f8469202ff8461c4f44629
Mushi <144794503+Mouche00@users.noreply.github.com>
2024-11-19 11:40:42 +0100

    [JBC-3] feature: initialize github repository

---
+++ src/main/java/org/citronix/JB9XCitronixApplication.java
@@ -0,0 +1,13 @@
+package org.citronix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JB9XCitronixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JB9XCitronixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/citronix/JB9XCitronixApplicationTests.java
@@ -0,0 +1,13 @@
+package org.citronix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JB9XCitronixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
